18cc2bfe1fc0908e9e837dfea58c47df8ce4c89a
courtlandcampo <courtland.campo@ufl.edu>
2022-06-17 12:28:44 -0400

    Adjusted ARIA expanded atrributes

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 var hamburgerBtnEl = document.querySelector(".hamburger-btn")
 var menuEl = document.querySelector("#main-menu")
-var ariaValue = document.querySelector(".hamburger-btn").getAttribute("aria-expanded")
+//var ariaValue = document.querySelector(".hamburger-btn").getAttribute("aria-expanded")
 
-hamburgerBtnEl.onclick = function() {
+
+hamburgerBtnEl.addEventListener('click',function(){
+    if (menuEl.classList.contains('showmenu')){
+        collapseMenu()
+
+    } else {
+        openMenu()
+
+    }
+}) 
+
+function openMenu() {
     menuEl.classList.add('show-menu')
-    ariaValue = "true"
-    console.log(ariaValue)
+    hamburgerBtnEl.setAttribute('aria-expanded', 'true')
 }
+
+function collapseMenu() {
+    menuEl.classList.remove('show-menu')
+    hamburgerBtnEl.setAttribute('aria-expanded', 'false')
+} 
